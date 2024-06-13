f70a0385c4eaa42b3a7429430eb51e6afad13291
olwe2shezi <116948429+olwe2shezi@users.noreply.github.com>
2024-06-13 20:02:19 +0200

    working on cutscene

---
+++ INTERACTIVE NARRATIVE/Assets/SCRIPTS/Cutscene/CutsceneController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CutsceneController : MonoBehaviour
+{
+    public TextAnimator textAnimator;
+    public Button continueButton;
+    public Image background; // Add this line to reference the background image
+    public Sprite[] backgroundImages; // Add this line to reference the background images
+
+    private string[] dialogueLines = {
+        "It's Prom night at Pretoria High School! And all the Matriculants are looking lavish",
+        "But wait...",
+        "Xola, one of the prom queen nominees has been...",
+        "MURDERED.",
+        "As you’ve been assigned to investigate this case, you’ve heard rumours that you are next in line to get a promotion and be ranked a Police Corporal at your station",
+        "so you deem solving this case as your one shot to solidify that promotion by unravelling the mystery.",
+        "Bring the culprit to justice before the night is over."
+    };
+
+    private int currentLineIndex = 0;
+
+    void Start()
+    {
+        continueButton.onClick.AddListener(OnContinueButtonClicked);
+        ShowNextDialogue();
+    }
+
+    void OnContinueButtonClicked()
+    {
+        if (currentLineIndex < dialogueLines.Length - 1)
+        {
+            currentLineIndex++;
+            ShowNextDialogue();
+        }
+        else
+        {
+            // End of dialogue or transition to next scene
+            Debug.Log("Cutscene finished!");
+        }
+    }
+
+    void ShowNextDialogue()
+    {
+        textAnimator.StartTyping(dialogueLines[currentLineIndex]);
+
+        // Update the background image if it exists
+        if (currentLineIndex < backgroundImages.Length)
+        {
+            background.sprite = backgroundImages[currentLineIndex];
+        }
+    }
+}
+
+
---
+++ INTERACTIVE NARRATIVE/Assets/SCRIPTS/Cutscene/TextAnimator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextAnimator : MonoBehaviour
+{
+    public Text dialogueText;
+    public float typingSpeed = 0.05f;
+    private string currentText = "";
+
+    public void StartTyping(string text)
+    {
+        currentText = text;
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+        dialogueText.text = "";
+        foreach (char letter in currentText.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+}
+
